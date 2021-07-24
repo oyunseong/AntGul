284d5be242ed3a043aca6b44820b99133f5e9cf7
oyunseong <gkrlsanjf@naver.com>
2021-07-24 19:24:11 +0900

    home, board fragment 구현

--- app/src/main/java/com/antgul/antgul_android/MainActivity.java
+++ app/src/main/java/com/antgul/antgul_android/MainActivity.java
@@ -2,13 +2,24 @@ package com.antgul.antgul_android;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.antgul.antgul_android.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import static com.antgul.antgul_android.R.id.menu_board;
+import static com.antgul.antgul_android.R.id.menu_home;
+import static com.antgul.antgul_android.R.id.menu_my_page;
+import static com.antgul.antgul_android.R.id.menu_notion;
+import static com.google.android.material.bottomnavigation.BottomNavigationView.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +28,10 @@ public class MainActivity extends AppCompatActivity {
     private final int FRAGMENT_BOARD = 2;
 
     private ActivityMainBinding binding;
-
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private HomeActivity homeActivity;
+    private BoardActivity boardActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +40,31 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
-        // 홈버튼 클릭
-        binding.bottomNav.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
+
+        binding.bottomNav.setOnNavigationItemSelectedListener(new OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.menu_home:
+                        callFragment(0);
+                        break;
+                    case menu_board:
+                        callFragment(1);
+                        break;
+                    case menu_notion:
+                        callFragment(2);
+                        break;
+                    case menu_my_page:
+                        callFragment(3);
+                }
+                return true;
             }
         });
 
-        activity_main_home_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.e("onClick","home button");
-                callFragment(FRAGMENT_HOME);
-            }
-        });
-        activity_main_board_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.e("onClick","board button");
-                callFragment(FRAGMENT_BOARD);
-            }
-        });
+        homeActivity = new HomeActivity();
+        boardActivity = new BoardActivity();
 
         // 액티비티 호출 시점에 있는 어느 플래그먼트를 프레임레이아웃에 올릴건지 정함
         callFragment(FRAGMENT_HOME);
@@ -56,21 +73,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void callFragment(int index) {
         // 프래그먼트 사용을 위해
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        fragmentManager= getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
 
         switch (index) {
             // HomeActivity 프래그먼트 호출
-            case 1:
-                HomeActivity homeActivity = new HomeActivity();
-                transaction.replace(R.id.frame_layout, homeActivity);
-                transaction.addToBackStack(null);
-                transaction.commit();
+            case 0:
+                fragmentTransaction.replace(binding.fragmentFrame.getId(),homeActivity);
+                fragmentTransaction.commit();
                 break;
-            case 2:
-                BoardActivity boardActivity = new BoardActivity();
-                transaction.replace(R.id.frame_layout, boardActivity);
-                transaction.addToBackStack(null);
-                transaction.commit();
+            case 1:
+                fragmentTransaction.replace(binding.fragmentFrame.getId(),boardActivity);
+                fragmentTransaction.commit();
                 break;
         }
     }
