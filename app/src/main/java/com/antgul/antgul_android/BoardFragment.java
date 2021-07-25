4db22cb284c9dae4f094b3f5098e3e25ce5d1a40
oyunseong <gkrlsanjf@naver.com>
2021-07-25 18:59:01 +0900

    commit

--- app/src/main/java/com/antgul/antgul_android/BoardFragment.java
+++ app/src/main/java/com/antgul/antgul_android/BoardFragment.java
@@ -1,5 +1,6 @@
 package com.antgul.antgul_android;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,27 +11,46 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.antgul.antgul_android.base.BaseFragment;
 import com.antgul.antgul_android.databinding.FragmentBoardBinding;
+import com.antgul.antgul_android.databinding.FragmentNotionBinding;
 
-public class BoardFragment extends Fragment {
+public class BoardFragment extends BaseFragment<FragmentBoardBinding> {
     private MainActivity mainActivity;
     private FragmentBoardBinding binding;
 
+    // 액티비티위에 올라오는 순간 호출됩니다.
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
 
-    public BoardFragment(){
+        activity = (MainActivity)getActivity();
+        showToast("게시판 프래그먼트 호출");
+
 
     }
 
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+        activity
+    }
+
+    @Override
+    protected FragmentBoardBinding getViewBinding(@NonNull LayoutInflater inflater, @Nullable ViewGroup container) {
+        return null;
+    }
+
+    @Override
+    protected void setUpView() {
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull  LayoutInflater inflater, @Nullable  ViewGroup container, @Nullable  Bundle savedInstanceState) {
-        binding.testbtn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getContext(),"클릭",Toast.LENGTH_SHORT).show();
-            }
-        });
+
 
         return inflater.inflate(R.layout.fragment_board,container,false);
     }
