50de18039c2763a7cacda11a1c89c773a329626b
oyunseong <gkrlsanjf@naver.com>
2021-07-29 23:47:10 +0900

    파이어베이스 연동 성공

--- app/src/main/java/com/antgul/antgul_android/BoardFragment.java
+++ app/src/main/java/com/antgul/antgul_android/BoardFragment.java
@@ -53,16 +53,16 @@ public class BoardFragment extends BaseFragment<FragmentBoardBinding> {
 
     // viewPager2 실행 메서드
     private void setInit() {
-        final float pageMargin = (float) getResources().getDimensionPixelOffset(R.dimen.pageMargin); // 페이지끼리 보이는 간격
-        final float pageOffset = (float) getResources().getDimensionPixelOffset(R.dimen.pageOffset); // 페이지 보이는 정도
+        final float pageMargin = (float) getResources().getDimensionPixelOffset(R.dimen.pageMargin);// 페이지끼리 보이는 간격
+        final float pageOffset = (float) getResources().getDimensionPixelOffset(R.dimen.pageOffset);// 페이지 보이는 정도
         String[] titles = new String[]{"전체", "관심종목","테스트1","테스트2","테스트3"}; // tab title
 
-        ViewPager2 viewPager2 = binding.boardViewpager;        // 뷰페이저를 참조합니다.
-        ViewPagerBoardAdapter viewPagerBoardAdapter = new ViewPagerBoardAdapter(getActivity());// 프래그먼트에서는 getActivity로 참조합니다.
-        viewPager2.setAdapter(viewPagerBoardAdapter);// 어댑터를 파라미터로 받고 viewpager2에 전달합니다.
-        viewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL); // 방향은 가로로 설정합니다.
-        viewPager2.setOffscreenPageLimit(6); // 페이지 개수 한정
-        viewPager2.setCurrentItem(100); // 무제한 스크롤 처럼 보이기
+        ViewPager2 viewPager2 = binding.boardViewpager;                                             // 뷰페이저를 참조합니다.
+        ViewPagerBoardAdapter viewPagerBoardAdapter = new ViewPagerBoardAdapter(getActivity());     // 프래그먼트에서는 getActivity로 참조합니다.
+        viewPager2.setAdapter(viewPagerBoardAdapter);                                               // 어댑터를 파라미터로 받고 viewpager2에 전달합니다.
+        viewPager2.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);                               // 방향은 가로로 설정합니다.
+        viewPager2.setOffscreenPageLimit(6);                                                        // 페이지 개수 한정
+        viewPager2.setCurrentItem(100);                                                             // 무제한 스크롤 처럼 보이기
 
         viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
@@ -92,7 +92,7 @@ public class BoardFragment extends BaseFragment<FragmentBoardBinding> {
         });
         /*
         *    displaying tabLayout
-        *    attach 메서드가 ViewPage2가 어댑터를 가진후에 호출되기 때문에 어댑터에 setAdapter해준후에
+        *    attach 메서드가 ViewPage2가 어댑터를 가진후에 호출되기 때문에 어댑터에 setAdapter 적용 후에
         *    TabLayoutMediator 를 선언해야합니다.
         */
         new TabLayoutMediator(binding.boardTabLayout, viewPager2,(tab, position) -> tab.setText(titles[position])).attach();
