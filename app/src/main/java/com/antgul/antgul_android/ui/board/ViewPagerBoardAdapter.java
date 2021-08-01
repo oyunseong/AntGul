366c4e1fbac3e27d6ea7dc7ce8963efe01e47881
oyunseong <gkrlsanjf@naver.com>
2021-08-01 23:43:40 +0900

    commit

--- app/src/main/java/com/antgul/antgul_android/ui/board/ViewPagerBoardAdapter.java
+++ app/src/main/java/com/antgul/antgul_android/ui/board/ViewPagerBoardAdapter.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
+import com.antgul.antgul_android.FreeBoardFragment;
 import com.antgul.antgul_android.TestFragment;
 
 public class ViewPagerBoardAdapter extends FragmentStateAdapter {
