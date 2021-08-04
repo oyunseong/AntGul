b89419b86076292f823ed168f4980f3613b2f226
oyunseong <gkrlsanjf@naver.com>
2021-08-04 20:19:16 +0900

    이메일, 비밀번호, 닉네임 정규식 수정 및 회원가입 중 발생하는 예외 처리

--- app/src/main/java/com/antgul/antgul_android/ui/join/SignUpActivity.java
+++ app/src/main/java/com/antgul/antgul_android/ui/join/SignUpActivity.java
@@ -63,41 +63,49 @@ public class SignUpActivity extends BaseActivity<ActivitySignUpBinding> {
     private void validateCreateUser() {
         String inputEmail = binding.etId.getText().toString();
         String inputPassword = binding.etPw.getText().toString();
+        String inputPasswordConfirm = binding.etConfirmPw.getText().toString();
+        String inputNick = binding.etNickName.getText().toString();
 
-        // 이메일형식 체크, 비밀번호 8~20자리 + 특수문자 포함
         String regexEmail = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\\w+\\.)+\\w+$";
-        String regexPw = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{8,20}$";
+        String regexPw = "^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}$";                    // 숫자/영문/특수문자를 최소 1개를 포함하고 공백은 허용되지 않음 8~16글자
+        String regexNick = "^[가-힣a-zA-Z0-9]{2,12}$";                                               // 한글/영문/숫자 포함 2~12
+
         Pattern patternEmail = Pattern.compile(regexEmail);
+        Pattern patternNick = Pattern.compile(regexNick);
         Pattern patternPw = Pattern.compile(regexPw);
 
-        Matcher matcherId = patternEmail.matcher(inputEmail);
+        Matcher matcherEmail = patternEmail.matcher(inputEmail);
+        Matcher matcherNick = patternNick.matcher(inputNick);
         Matcher matcherPw = patternPw.matcher(inputPassword);
 
-        // email,pw == true
-        if (matcherId.matches() && matcherPw.matches()) {
-            Toast.makeText(this, "회원가입 성공", Toast.LENGTH_SHORT).show();
-            createUser(inputEmail, inputPassword);
+
+        //TODO 회원가입 정규식 알고리즘 수정
+
+        if (matcherEmail.matches() && matcherPw.matches() && matcherNick.matches() && inputPassword.equals(inputPasswordConfirm)) {
+            Toast.makeText(this, "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+            createUser(inputEmail, inputPassword, inputNick);
             finish();
-            // email == true, pw == false
-        } else if (matcherId.matches() && !matcherPw.matches()) {
-            Toast.makeText(this, "비밀번호 형식이 다릅니다.", Toast.LENGTH_SHORT).show();
-            binding.etPw.getText().clear();
-            binding.etConfirmPw.getText().clear();
-            // email == false , pw == true
-        } else if (!matcherId.matches() && matcherPw.matches()) {
-            Toast.makeText(this, "이메일 형식이 다릅니다.", Toast.LENGTH_SHORT).show();
-            binding.etId.getText().clear();
-            // email, pw  == false
-        } else {
-            Toast.makeText(this, "이메일, 비밀번호 형식이 다릅니다.", Toast.LENGTH_SHORT).show();
-            binding.etId.getText().clear();
-            binding.etPw.getText().clear();
-            binding.etConfirmPw.getText().clear();
+        } else if (inputEmail.equals("")) {
+            showToast("이메일을 입력해주세요");
+        } else if (!matcherEmail.matches()) {
+            showToast("이메일 형식에 어긋납니다");
+        } else if (inputNick.equals("")) {
+            showToast("닉네임을 입력해주세요");
+        } else if (!matcherNick.matches()) {
+            showToast("닉네임 형식에 어긋납니다");
+        } else if (inputPassword.equals("")) {
+            showToast("비밀번호를 입력해주세요");
+        } else if (!matcherPw.matches()) {
+            showToast("비밀번호 형식에 어긋납니다");
+        } else if (inputPasswordConfirm.equals("")) {
+            showToast("비밀번호 확인을 입력해주세요");
+        } else if (!inputPassword.equals(inputPasswordConfirm)) {
+            showToast("비밀번호가 일치하지 않습니다.");
         }
     }
 
     //TODO 프로그레스바 추가
-    private void createUser(String email, String password) {
+    private void createUser(String email, String password, String nickName) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -125,6 +133,7 @@ public class SignUpActivity extends BaseActivity<ActivitySignUpBinding> {
             finish();
             startActivity(new Intent(getApplicationContext(), MainActivity.class)); //TODO 로그인 화면에 이메일 넣어주기. Intent 로 통신 가능. 검색어 : 액티비티간 데이터 전달(통신)
 
+            // 회원가입 실패했을 때 ( 1. 네트워크 연결 실패 시 발생 )
         } else {
             Toast.makeText(this, "updateUI Error", Toast.LENGTH_SHORT).show();
         }
